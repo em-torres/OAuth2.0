089e0ddb06a6884de9a2ae1fc18da717145249ff
Edwin Marte <kdgpro15@gmail.com>
2017-06-25 10:41:26 -0400

    Complying with PEP8 Standards

--- Vagrantfile
+++ Vagrantfile
@@ -7,7 +7,7 @@ VAGRANTFILE_API_VERSION = "2"
 Vagrant.configure(VAGRANTFILE_API_VERSION) do |config|
   config.vm.provision :shell, path: "pg_config.sh"
   config.vm.box = "bento/ubuntu-16.04-i386"
-  config.vm.network "forwarded_port", guest: 5040, host: 5040
+  config.vm.network "forwarded_port", guest: 5000, host: 5000
   
   # Work around disconnected virtual network cable.
   config.vm.provider "virtualbox" do |vb|
--- database_setup.py
+++ database_setup.py
@@ -5,6 +5,7 @@ from sqlalchemy import create_engine
  
 Base = declarative_base()
 
+
 class Restaurant(Base):
     __tablename__ = 'restaurant'
    
@@ -13,39 +14,35 @@ class Restaurant(Base):
 
     @property
     def serialize(self):
-       """Return object data in easily serializeable format"""
+       """ Return object data in easily serializeable format """
        return {
            'name'         : self.name,
            'id'           : self.id,
        }
- 
+
+
 class MenuItem(Base):
     __tablename__ = 'menu_item'
 
-
-    name =Column(String(80), nullable = False)
-    id = Column(Integer, primary_key = True)
+    name =Column(String(80), nullable=False)
+    id = Column(Integer, primary_key=True)
     description = Column(String(250))
     price = Column(String(8))
     course = Column(String(250))
-    restaurant_id = Column(Integer,ForeignKey('restaurant.id'))
+    restaurant_id = Column(Integer, ForeignKey('restaurant.id'))
     restaurant = relationship(Restaurant)
-
 
     @property
     def serialize(self):
-       """Return object data in easily serializeable format"""
+       """ Return object data in easily serializeable format """
        return {
            'name'         : self.name,
-           'description'         : self.description,
-           'id'         : self.id,
-           'price'         : self.price,
-           'course'         : self.course,
+           'description'  : self.description,
+           'id'           : self.id,
+           'price'        : self.price,
+           'course'       : self.course,
        }
 
-
-
 engine = create_engine('sqlite:///restaurantmenu.db')
- 
 
 Base.metadata.create_all(engine)
